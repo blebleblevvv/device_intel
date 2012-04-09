29b60b58797dc345fa17a086bcd7f14bb630ca5a
srinivas pandruvada <srinivas.pandruvada@intel.com>
2012-04-09 11:08:39 -0700

    Audio Enabling
    Added audioconf.rc file and included target specific Audio HAL
    in packages.
    Added necessary audio modules for PC platform.
    
    Change-Id: Ic99b0dc1dd770390f698771ce35326050f74b9a9
    Signed-off-by: srinivas pandruvada <srinivas.pandruvada@intel.com>

--- products/pc_std_common.mk
+++ products/pc_std_common.mk
@@ -14,3 +14,11 @@ PRODUCT_PACKAGES += \
 	tinymix \
 	tinyplay \
 	tinycap \
+
+# PC std common files
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/asound.conf:system/etc/asound.conf \
+
+# audio support
+PRODUCT_PACKAGES += \
+	audio.primary.$(TARGET_PRODUCT) \
