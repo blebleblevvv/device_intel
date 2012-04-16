a7e94a9d04e21ba3946e618fad108563f3128977
Andrew Boie <andrew.p.boie@intel.com>
2012-04-16 15:55:05 -0700

    add Init logos for Samsung and Lenovo
    Both devices coincidentally have 1366x768 framebuffers.
    
    Depends-Change-Id: Idd7a2bb80c703b0b4a435f6fdc82ff542dd5cfb4
    Change-Id: I6d595b2238a5c364393d8a6e9e9ebb4c04d38445
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>

--- lenovox220t/lenovox220t.mk
+++ lenovox220t/lenovox220t.mk
@@ -11,6 +11,7 @@ PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/init.lenovox220t.sh:system/etc/init.lenovox220t.sh \
 	$(LOCAL_PATH)/Wacom_ISDv4_E6_Finger.idc:system/usr/idc/Wacom_ISDv4_E6_Finger.idc \
 	$(LOCAL_PATH)/Wacom_ISDv4_E6_Pen.idc:system/usr/idc/Wacom_ISDv4_E6_Pen.idc \
+	device/intel/common/intel_initlogo_1366x768.rle:root/initlogo.rle \
 
 # Additional configurations needed for the platform.
 # Mostly binary files (e.g. firmware)
--- samsungxe700t/samsungxe700t.mk
+++ samsungxe700t/samsungxe700t.mk
@@ -11,6 +11,7 @@ PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/init.samsungxe700t.sh:system/etc/init.samsungxe700t.sh \
 	$(LOCAL_PATH)/Atmel_Atmel_maXTouch_Digitizer.idc:system/usr/idc/Atmel_Atmel_maXTouch_Digitizer.idc \
 	$(LOCAL_PATH)/Wacom_ISDv4_EC_Pen.idc:system/usr/idc/Wacom_ISDv4_EC_Pen.idc \
+	device/intel/common/intel_initlogo_1366x768.rle:root/initlogo.rle \
 
 # Additional configurations needed for the platform.
 # Mostly binary files (e.g. firmware)
