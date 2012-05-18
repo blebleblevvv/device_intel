55fd4bce41c73450a4802085d46b91aa2c8e92ec
Daniel Leung <daniel.leung@intel.com>
2012-05-18 09:29:51 -0700

    Add the ability to create incremental OTA update file
    Under the ota-dev target, if OTA_INCREMENTAL_FROM is specified and
    points to to target_files.zip from previous release, an incremental
    OTA update archive will be created.
    
    Change-Id: I341ced5865f764ccb697d503003b76ab78894d8b
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>

--- build/tasks/80-ota.mk
+++ build/tasks/80-ota.mk
@@ -9,3 +9,31 @@ $(ota_package): $(BUILT_TARGET_FILES_PACKAGE) $(OTATOOLS)
 
 .PHONY: ota-dev
 ota-dev: $(ota_package)
+
+
+# If OTA_INCREMENTA_FROM is specified as make parameter, then otapackage will
+# also create an incremental package from the file specified by the value of
+# this parameter.
+# For example:
+#  make ... ota-dev-inc OTA_INCREMENTAL_FROM=/path/to/previous-target-files-package.zip
+ifdef OTA_INCREMENTAL_FROM
+
+ota_inc_name := $(TARGET_PRODUCT)
+ifeq ($(TARGET_BUILD_TYPE),debug)
+  ota_inc_name := $(ota_inc_name)_debug
+endif
+ota_inc_name := $(ota_inc_name)-ota-$(FILE_NAME_TAG)
+
+INTERNAL_OTA_INCREMENTAL_TARGET := $(PRODUCT_OUT)/$(ota_inc_name)-inc.zip
+$(INTERNAL_OTA_INCREMENTAL_TARGET): KEY_CERT_PAIR := $(DEFAULT_KEY_CERT_PAIR)
+$(INTERNAL_OTA_INCREMENTAL_TARGET): $(BUILT_TARGET_FILES_PACKAGE) $(OTATOOLS)
+	@echo "Package incremental OTA: $@"
+	$(hide) ./build/tools/releasetools/ota_from_target_files \
+	   --aslr_mode off --verbose \
+	   --path $(HOST_OUT) \
+	   --incremental_from $(OTA_INCREMENTAL_FROM) \
+	   $(BUILT_TARGET_FILES_PACKAGE) $@
+
+ota-dev: $(INTERNAL_OTA_INCREMENTAL_TARGET)
+
+endif # ifdef OTA_INCREMENTAL_FROM
