b49d76ccee2607cf3a213154f4b8909779d5671f
Jianxun Zhang <jianxun.zhang@intel.com>
2012-05-30 08:33:29 -0700

    Android IA: [camera] Enable Lenovo X220T's camera
    This commit enables camera on X220T and configure it
    as front camera.
    
    No source code change in camera HAL required.
    
    For: AIA-506
    Change-Id: If5460d5a3f1620447c69fa63af0c95d6389de7a4
    Signed-off-by: Jianxun Zhang <jianxun.zhang@intel.com>

--- lenovox220t/BoardConfig.mk
+++ lenovox220t/BoardConfig.mk
@@ -37,6 +37,24 @@ BOARD_HAVE_WIFI := true
 
 ADDITIONAL_DEFAULT_PROPERTIES += wifi.interface=wlan0
 
+# setup additional build prop for product
+
+# camera props
+# camera.n: n is camera ID to android
+# camera.devname: device name of node, e.g. /dev/video0
+# number: number of cameras on board. If you claim more than two
+#         cameras, Adroid (ICS) will limit it to 2 in run time.
+# facing: [front/back]
+# orientation: [0/90/180/270]
+# If any field is missed or fed with an invalid value, NONE of
+# cameras will be reported to Android.
+
+ADDITIONAL_BUILD_PROPERTIES += \
+	ro.camera.number=1 \
+	ro.camera.0.devname=/dev/video0 \
+	ro.camera.0.facing=front \
+	ro.camera.0.orientation=0 \
+
 # Product specific overlay - uncomment this if/when an overlay
 # or part of an overlay needs to apply to this target only
 PRODUCT_PACKAGE_OVERLAYS := device/intel/lenovox220t/overlay
