854351e331292680c0d56d7f632bedf75dd5137d
Andrew Boie <andrew.p.boie@intel.com>
2012-07-10 15:07:33 -0700

    MUST_REVERT: disable recovery UI lib
    API changes broke it, fix later.
    
    Change-Id: Icbb630e5249f042cd69c5a1180090904d36459b9
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>

--- pc_std/recovery/Android.mk
+++ pc_std/recovery/Android.mk
@@ -1,10 +1,10 @@
 LOCAL_PATH := $(call my-dir)
 
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := minimal_recovery_ui.c
-LOCAL_MODULE_TAGS := eng
-LOCAL_C_INCLUDES := bootable/recovery
-LOCAL_MODULE := libminimal_recovery_ui
-LOCAL_CFLAGS := -Wall -Werror -Wno-unused-parameter
-include $(BUILD_STATIC_LIBRARY)
+#include $(CLEAR_VARS)
+#LOCAL_SRC_FILES := minimal_recovery_ui.c
+#LOCAL_MODULE_TAGS := eng
+#LOCAL_C_INCLUDES := bootable/recovery
+#LOCAL_MODULE := libminimal_recovery_ui
+#LOCAL_CFLAGS := -Wall -Werror -Wno-unused-parameter
+#include $(BUILD_STATIC_LIBRARY)
 
