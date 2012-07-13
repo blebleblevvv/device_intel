b3152c26a71cf043e6fcede835ca7258713126a0
shaun <shaunx.v.savage@intel.com>
2012-07-13 13:55:48 -0700

    SATA link power mgnt
    Reduce power for SATA all devices by putting them in low power mode
    
    Issue: AXIA-101
    Change-Id: Ie9a828f7983600d9b97dc1ce85c5a9885d95eed4
    Signed-off-by: shaun <shaunx.v.savage@intel.com>

--- pc_std/init.pc_std.sh
+++ pc_std/init.pc_std.sh
@@ -8,4 +8,11 @@
 lid=`getprop init.panel_ignore_lid`
 [ "$lid" != "" ] && echo $lid > /sys/module/i915/parameters/panel_ignore_lid
 
+for dirname in /sys/class/scsi_host/host* ; do
+        if [ -e $dirname/link_power_management_policy ]
+        then
+                echo min_power > $dirname/link_power_management_policy
+        fi
+done
+
 exit 0
