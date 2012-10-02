76546e522929af875731757f9bbc0b5f43ec2941
Andrew Boie <andrew.p.boie@intel.com>
2012-10-02 12:47:26 -0700

    wifi: Move boilerplate into common Makefile
    No point in duplicating all this stuff for every BoardConfig;
    it's valid for any product which uses iwlwifi.
    
    BOARD_HAVE_WIFI not used anywhere; remove.
    
    Depends-Change-Id: I02ba9f28f7e2c8d0ab63b22a3bc424f11f7a96eb
    Change-Id: I828d8ad7530de6c1afe8ca7f164b59c22ee333f9
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>

--- bigcore/BoardConfig-iwlwifi.mk
+++ bigcore/BoardConfig-iwlwifi.mk
@@ -89,3 +89,29 @@ ifneq ($(CALLED_FROM_SETUP),true)
   $(info WIFI: using driver module basename "$(wifi_driver_basename)")
 endif
 endif
+
+# Tells Android HAL how to load WIFI driver.
+# See hardware/libhardware_legacy/wifi/{Android.mk,wifi.c}
+WIFI_DRIVER_PROBE_PATH := /system/lib/modules/
+WIFI_DRIVER_MODULE_NAME := $(wifi_driver_basename)
+
+# Workarounds for excessive errors that interfere with proper wifi
+# operation in the IWLWIFI driver:
+#
+# plcp_check=N  Works around excessive PLCP health-check failures
+#               that cause continuous resetting of RF
+# 11n_disable=1 Works around excessive tx retransmits while 11n
+#               is enabled, symptom: name-resolve failure
+IWLWIFI_PARMLIST := \
+       plcp_check=N \
+       11n_disable=1 \
+
+WIFI_DRIVER_MODULE_ARG = "$(strip $(IWLWIFI_PARMLIST))"
+ADDITIONAL_DEFAULT_PROPERTIES += wifi.interface=wlan0
+
+# Must be set to enable WPA supplicant build. WEXT is the driver for generic
+# linux wireless extensions. NL80211 supercedes it.
+# This variable is used by external/wpa_supplicant/Android.mk
+BOARD_WPA_SUPPLICANT_DRIVER=NL80211
+WPA_SUPPLICANT_VERSION:=VER_0_8_X
+
--- lenovox220t/BoardConfig.mk
+++ lenovox220t/BoardConfig.mk
@@ -7,34 +7,8 @@ TARGET_ARCH_VARIANT := sandybridge
 # The board name as known by Android SDK
 TARGET_BOOTLOADER_BOARD_NAME := lenovox220t
 
-# Must be set to enable WPA supplicant build. WEXT is the driver for generic
-# linux wireless extensions. NL80211 supercedes it.
-# This variable is used by external/wpa_supplicant/Android.mk
-BOARD_WPA_SUPPLICANT_DRIVER=NL80211
-WPA_SUPPLICANT_VERSION:=VER_0_8_X
-
-# set the wifi_driver_basename variable
-include device/intel/bigcore/select_wifi_driver.mk
-
-# Tells Android HAL how to load WIFI driver.
-# See hardware/libhardware_legacy/wifi/{Android.mk,wifi.c}
-WIFI_DRIVER_PROBE_PATH := /system/lib/modules/
-WIFI_DRIVER_MODULE_NAME := $(wifi_driver_basename)
-# Workarounds for excessive errors that interfere with proper wifi
-# operation in the IWLWIFI driver:
-#
-# plcp_check=N  Works around excessive PLCP health-check failures
-#               that cause continuous resetting of RF
-# 11n_disable=1 Works around excessive tx retransmits while 11n
-#               is enabled, symptom: name-resolve failure
-IWLWIFI_PARMLIST := \
-	plcp_check=N \
-	11n_disable=1 \
-
-WIFI_DRIVER_MODULE_ARG := "$(strip $(IWLWIFI_PARMLIST))"
-BOARD_HAVE_WIFI := true
-
-ADDITIONAL_DEFAULT_PROPERTIES += wifi.interface=wlan0
+# Use Intel wifi
+include device/intel/bigcore/BoardConfig-iwlwifi.mk
 
 # setup additional build prop for product
 
