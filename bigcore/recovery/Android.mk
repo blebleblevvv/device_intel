bf6ec5a7a96e54a921a969966acbf7ed8b0b1fe3
Andrew Boie <andrew.p.boie@intel.com>
2012-11-14 11:34:08 -0800

    remove -Werror from recovery ui plugin
    The plugin headers have warnings =/
    
    Change-Id: I6c7578e803c0916d367e4cd8e6fbadbbb69c4f23
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>

--- bigcore/recovery/Android.mk
+++ bigcore/recovery/Android.mk
@@ -5,5 +5,5 @@ LOCAL_SRC_FILES := bigcore_device.cpp
 LOCAL_MODULE_TAGS := eng
 LOCAL_C_INCLUDES := bootable/recovery
 LOCAL_MODULE := libpcstd_recovery_ui
-LOCAL_CFLAGS := -Wall -Werror
+LOCAL_CFLAGS := -Wall
 include $(BUILD_STATIC_LIBRARY)
