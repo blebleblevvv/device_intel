d0958255c2f2ac81604e19b6189c64c80f54770e
Daniel Leung <daniel.leung@intel.com>
2012-11-16 09:31:29 -0800

    Remove hardware permission for camera w/ autofocus and flash
    Remove the reference to the xml file containing permission for camera
    with autofocus and flash. The USB cameras we have do not support
    external autofocus request and no flash support.
    
    Change-Id: Ie57657307a753e1ace9c6fec984efc0c3180ef68
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>

--- common/generic_no_telephony.mk
+++ common/generic_no_telephony.mk
@@ -87,7 +87,6 @@ PRODUCT_COPY_FILES += \
         frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml \
         frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
         frameworks/native/data/etc/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
-        frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
         frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
         frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
         frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
