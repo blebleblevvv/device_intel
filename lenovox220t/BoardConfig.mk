12a0bdff1f7230eb57c14a33be7b7403cd87a95b
Matt Gumbel <matthew.k.gumbel@intel.com>
2012-12-26 14:23:39 -0800

    MUST_REVERT: use diskinstaller for lenovox220t
    Iago is producing a non-bootable device on the x220t (AXIA-1473). This
    is a temporary workaround for the issue.
    
    Change-Id: Ic91129b5b753118f999d19f9dfe64daa63232470
    Signed-off-by: Matt Gumbel <matthew.k.gumbel@intel.com>

--- lenovox220t/BoardConfig.mk
+++ lenovox220t/BoardConfig.mk
@@ -16,6 +16,9 @@ TARGET_ARCH_VARIANT := sandybridge
 # If any field is missed or fed with an invalid value, NONE of
 # cameras will be reported to Android.
 
+BOARD_KERNEL_CMDLINE := $(filter-out androidboot.bcb_device=/dev/block/by-name/misc, $(BOARD_KERNEL_CMDLINE))
+BOARD_KERNEL_CMDLINE += androidboot.bcb_device=/dev/block/sda6
+
 ADDITIONAL_BUILD_PROPERTIES += \
 	ro.camera.number=1 \
 	ro.camera.0.devname=/dev/video0 \
@@ -28,5 +31,19 @@ PRODUCT_PACKAGE_OVERLAYS += device/intel/lenovox220t/overlay
 
 # Do not use the platform sensor library
 BOARD_USE_PLATFORM_SENSOR_LIB := false
+
+# Causes bootable/diskinstaller/config.mk to be included which enables the
+# installer_img build target.  For more information on the installer, see
+# http://otc-android.intel.com/wiki/index.php/Installer
+TARGET_USE_DISKINSTALLER := true
+
+ifeq ($(TARGET_STAGE_DROIDBOOT),true)
+TARGET_DISKINSTALLER_CONFIG := device/intel/samsungxe700t/installer-fastboot.conf
+else
+TARGET_DISKINSTALLER_CONFIG := device/intel/samsungxe700t/installer.conf
+endif
+
+# Defines a partitioning scheme for the installer:
+TARGET_DISK_LAYOUT_CONFIG := device/intel/samsungxe700t/disk_layout.conf
 
 # end of file
