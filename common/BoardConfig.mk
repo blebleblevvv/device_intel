32c18177c823fb09395d7f6b3cbedbe471e73f49
Matt Gumbel <matthew.k.gumbel@intel.com>
2013-03-05 11:46:02 -0800

    common: revert to AOSP's perf
    external/perf/ depends on Bison 2.5. However, the bison that comes
    with Android is 2.3. In external/perf/Android.mk, bison is called
    using a relative path and thus will pick up the 2.3 version from out/
    if it has been built, otherwise it picks up the version from the host
    Linux distro. This means that perf sometimes builds and sometimes fails
    to build.
    
    For now, we downgrade to use perf from AOSP which builds fine using
    Bison 2.3.
    
    Change-Id: I111229c786bbc0de527f05ae507bb53376d89c6e
    Signed-off-by: Matt Gumbel <matthew.k.gumbel@intel.com>

--- common/BoardConfig.mk
+++ common/BoardConfig.mk
@@ -103,8 +103,11 @@ endif
 # Choose the version of perf to build based on the relative path
 # in the android tree (external/linux-tools-perf or external/perf)
 # If it is not set, external/linux-tools-perf will be built.
-ifneq ($(BOARD_PERF),external/linux-tools-perf)
-  BOARD_PERF := external/perf
+#
+# For now, AOSP's perf (linux-tools-perf) must be used because of a
+# bison 2.5 incompatability in external/perf/.
+ifeq ($(BOARD_PERF),)
+  BOARD_PERF := external/linux-tools-perf
 endif
 
 # Common libraries for the OTA/recovery mechanism
